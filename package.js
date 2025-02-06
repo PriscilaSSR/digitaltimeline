72b0d7d63560f7347caafa0c84f2b7b186be15c7
Priscila Schilaro Santa Rosa <priscila.schilarosantarosa@ucf.edu>
2025-02-06 12:50:45 -0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "homepage": "https://yourusername.github.io/your-repo-name"
+}
+{
+  "scripts": {
+    "predeploy": "npm run build",
+    "deploy": "gh-pages -d build"
+  }
+}
+npm install gh-pages --save-dev
+npm run deploy
